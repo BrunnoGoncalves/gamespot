eff8bebe3799844c385596ba08ec988fca90929a
BrunnoGoncalves <117948929+BrunnoGoncalves@users.noreply.github.com>
2022-12-05 20:20:21 -0300

    Update index.php

--- final3/index.php
+++ final3/index.php
@@ -41,13 +41,7 @@ $genero = get_all_genero($conn);
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contatos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Sobre</a>
-        </li>
+	      </li>
         <li class="nav-item">
           <?php if (isset($_SESSION['user_id'])) {?>
            
